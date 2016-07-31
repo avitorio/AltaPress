fbc228f1aa1a750583991b99dc608f117cd0f2ec
Andre Vitorio <andre@vitorio.net>
2016-07-31 21:07:01 +0200

    Update front-page and include some new files

--- front-page.php
+++ front-page.php
@@ -1,48 +1,51 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Front page file.
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
  *
  * @package understrap
  */
 
 get_header(); ?>
 
-<div class="wrapper" id="page-wrapper">
+    <?php
+
+        get_sidebar('hero'); 
+
+        get_sidebar('statichero');
+    ?>
+
+    <div class="wrapper" id="page-wrapper">
     
-    <div  id="content" class="container">
+	    <div  id="content" class="container">
 
-        <div class="row">
-        
-    	   <div id="primary" class="col-md-12 content-area">
-           
-                 <main id="main" class="site-main" role="main">
+	        <div class="row">
+	        
+	    	   <div id="primary" class="col-md-12 content-area">
+	           
+	                 <main id="main" class="site-main" role="main">
 
-                    <?php while ( have_posts() ) : the_post(); ?>
+	                    <?php while ( have_posts() ) : the_post(); ?>
 
-                        <?php get_template_part( 'loop-templates/content', 'page' ); ?>
+	                        <?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-                        <?php
-                            // If comments are open or we have at least one comment, load up the comment template
-                            if ( comments_open() || get_comments_number() ) :
-                                comments_template();
-                            endif;
-                        ?>
+	                        <?php
+	                            // If comments are open or we have at least one comment, load up the comment template
+	                            if ( comments_open() || get_comments_number() ) :
+	                                comments_template();
+	                            endif;
+	                        ?>
 
-                    <?php endwhile; // end of the loop. ?>
+	                    <?php endwhile; // end of the loop. ?>
 
-                </main><!-- #main -->
-               
-    	    </div><!-- #primary -->
+	                </main><!-- #main -->
+	               
+	    	    </div><!-- #primary -->
 
-        </div><!-- .row -->
-        
-    </div><!-- Container end -->
-    
-</div><!-- Wrapper end -->
+	        </div><!-- .row -->
+	        
+	    </div><!-- Container end -->
+	    
+	</div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
